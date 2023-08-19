1af4cf4cf11b952cee4d34bf582ecdfcf1dbfac2
Jackson Carlos Leite Ramalho <jackson@MacBook-Pro-de-Jackson.local>
2023-08-19 08:12:57 -0300

    ajustando beans cliente

--- fastfood-management-system/fastfood-system-application/src/main/java/com/fiap/postech/fastfoodsystemapi/config/ClienteBeanConfiguration.java
+++ fastfood-management-system/fastfood-system-application/src/main/java/com/fiap/postech/fastfoodsystemapi/config/ClienteBeanConfiguration.java
@@ -1,8 +1,7 @@
 package com.fiap.postech.fastfoodsystemapi.config;
 
 import com.fiap.postech.fastfoodsystemcore.domain.entities.cliente.ClienteRepository;
-import com.fiap.postech.fastfoodsystemcore.domain.usecases.cliente.CadastroDeCliente;
-import com.fiap.postech.fastfoodsystemcore.domain.usecases.cliente.ListagemDeCliente;
+import com.fiap.postech.fastfoodsystemcore.domain.usecases.cliente.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,13 +15,28 @@ public class ClienteBeanConfiguration {
     }
 
     @Bean
-    public CadastroDeCliente cadastrarCliente(){
+    public CadastroDeCliente cadastroDeCliente(){
         return new CadastroDeCliente(clienteRepository);
     }
 
     @Bean
-    public ListagemDeCliente listarClientes(){
+    public ListagemDeCliente listagemDeClientes(){
         return new ListagemDeCliente(clienteRepository);
+    }
+
+    @Bean
+    public IdentificacaoDeCliente identificacaoDeCliente(){
+        return new IdentificacaoDeCliente(clienteRepository);
+    }
+
+    @Bean
+    public AtualizacaoDeCliente AtualizacaoDeCliente(){
+        return new AtualizacaoDeCliente(clienteRepository);
+    }
+
+    @Bean
+    public ExclusaoDeCliente exclusaoDeClientes(){
+        return new ExclusaoDeCliente(clienteRepository);
     }
 
 }
