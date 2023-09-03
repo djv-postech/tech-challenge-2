affea70d8fcdd4285ff4230ff8174b2e09b55991
Jackson Carlos Leite Ramalho <jackson@MacBook-Pro-de-Jackson.local>
2023-09-03 12:04:26 -0300

    adding email and cpf validation

--- fastfood-management-system/fastfood-system-core/src/main/java/com/fiap/postech/fastfoodsystemcore/domain/vo/Email.java
+++ fastfood-management-system/fastfood-system-core/src/main/java/com/fiap/postech/fastfoodsystemcore/domain/vo/Email.java
@@ -5,6 +5,10 @@ public class Email {
 	private final String endereco;
 
 	public Email(String endereco) {
+		if (endereco == null ||
+				!endereco.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
+			throw new IllegalArgumentException("Email inválido.");
+		}
 		this.endereco = endereco;
 	}
 	
